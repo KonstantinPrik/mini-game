7b2156387c0517dc22ce79e1a8ca963ee4a631c3
KonstantinPrick <prikkos1806@gmail.com>
2022-10-20 20:27:53 +0300

    create new project

---
+++ app.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board')
+const colors = ['#9370DB', '#6495ED', '##F4A460', '#00FF7F', '#DC143C', '#ADFF2F', '#FF69B4', '#808000']
+const SQUARES_NUMBER = 1600
+
+for (let i=0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => 
+        setColor(square))
+
+    square.addEventListener('mouseleave', () => 
+        removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 
+                               0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = 'none'
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
